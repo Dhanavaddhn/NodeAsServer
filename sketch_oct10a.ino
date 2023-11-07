20ea87111447e483da9dc0a15658bc7806c5b300
Dhanavaddhn <143820869+Dhanavaddhn@users.noreply.github.com>
2023-11-07 10:44:24 +0700

    Update sketch_oct10a.ino

--- sketch_oct10a.ino
+++ sketch_oct10a.ino
@@ -1,42 +1,100 @@
-#include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include <DHT.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);
+ESP8266WebServer server(80);
 
-float temperature;
-float humidity;
-DHT dht14(D4,DHT11);
+const int ledPin = D6;
+DHT dht14(D4, DHT11);
+const char* ssid = "PADHAN";
+const char* password = "123456789";
 
-void setup()
-{
-  temperature = 0;
-  humidity = 0;
-  Serial.begin(9600);
-  dht14.begin();
-
-  lcd.begin();
+void init_wifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop()
-{
-  temperature = (dht14.readHumidity());
-  humidity = (dht14.readTemperature());
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print("  Humidty: ");
-  Serial.print(humidity);
+void setup(void) {
+  Serial.begin(9600);
+  init_wifi();
 
-  lcd.setCursor(0, 0);
-  char humidityString[10];
-  sprintf(humidityString, "Hum: %.2f %%", humidity);
-  lcd.print(humidityString);
+  pinMode(ledPin, OUTPUT);
 
-  lcd.setCursor(0, 1);
-  char temperatureString[10];
-  sprintf(temperatureString, "Temp: %.2f C", temperature);
-  lcd.print(temperatureString);
+  dht14.begin();
 
-  delay(1000);
+  server.on("/", HTTP_GET, []() {
+    float temperature = dht14.readTemperature();
+    float humidity = dht14.readHumidity();
+
+    String html = "<html>";
+    html += "<body><div>";
+    html += "<h1>Web Server</h1>";
+    html += "<p>Temperature: <span id='temperature'>" + String(temperature, 2) + " Celsius</span></p>";
+    html += "<p>Humidity: <span id='humidity'>" + String(humidity, 2) + " %</span></p>";
+    html += "<p>LED Status: ";
+    if (digitalRead(ledPin) == HIGH) {
+      html += "<span style='color: green;'>On</span>";
+    } else {
+      html += "<span style='color: red;'>Off</span>";
+    }
+    html += "</p>";
+    html += "<form action='/TurnOn' method='get'><input type='submit' value='Turn On'></form>";
+    html += "<form action='/TurnOff' method='get'><input type='submit' id='turnOffButton' value='Turn Off'></form>";
+    html += "</div></body></html>";
+
+    html += "<script>";
+    html += "function updateData() {";
+    html += "  var xhr = new XMLHttpRequest();";
+    html += "  xhr.open('GET', '/data', true);";
+    html += "  xhr.onreadystatechange = function() {";
+    html += "    if (xhr.readyState == 4 && xhr.status == 200) {";
+    html += "      var data = JSON.parse(xhr.responseText);";
+    html += "      document.getElementById('temperature').textContent = data.temperature.toFixed(2) + ' Celsius';";
+    html += "      document.getElementById('humidity').textContent = data.humidity.toFixed(2) + ' %';";
+    html += "    }";
+    html += "  };";
+    html += "  xhr.send();";
+    html += "}";
+    html += "setInterval(updateData, 500);";
+    html += "</script>";
+
+    server.send(200, "text/html", html);
+  });
+
+  server.on("/TurnOn", HTTP_GET, []() {
+    digitalWrite(ledPin, HIGH);
+    server.sendHeader("Location", "/");
+    server.send(302, "text/plain", "");
+  });
+
+  server.on("/TurnOff", HTTP_GET, []() {
+    digitalWrite(ledPin, LOW);
+    server.sendHeader("Location", "/");
+    server.send(302, "text/plain", "");
+  });
+
+  server.on("/data", HTTP_GET, []() {
+    float temperature = dht14.readTemperature();
+    float humidity = dht14.readHumidity();
+
+    String data = "{\"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + "}";
+    server.send(200, "application/json", data);
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
 }
